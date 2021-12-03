76d6dd06035dbb6da8c2b0a9344dc7b08c81fba0
guiazevedo17 <gui.azevedo@ymail.com>
2021-12-03 02:42:10 -0300

    controllers adicionados corretamente

---
+++ src/controllers/CadastroJogadoresController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class CadastroJogadoresController {
+
+    @FXML
+    private TextField txtJogador1, txtJogador2;
+
+}
---
+++ src/controllers/MenuController.java
@@ -0,0 +1,10 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MenuController{
+
+    @FXML
+    private Button btnCadastrarJogador, btnIniciarPartida;
+}
