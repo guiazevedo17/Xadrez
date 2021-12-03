d3109a0fc714dcc8fb820f6b2061c1d4759e4c6f
guiazevedo17 <gui.azevedo@ymail.com>
2021-12-03 03:44:13 -0300

    menu e cadastro trocando telas, tabuleiro ainda n

--- src/controllers/MenuController.java
+++ src/controllers/MenuController.java
@@ -1,10 +1,35 @@
 package controllers;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class MenuController{
 
     @FXML
     private Button btnCadastrarJogador, btnIniciarPartida;
+
+    @FXML
+    public void cadastrarJogadores(ActionEvent event) throws IOException {
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        window.setScene(new Scene(FXMLLoader.load(getClass().getResource("../fxmls/CadastroJogadores.fxml"))));
+        window.setTitle("ChessQMate - CADASTRO JOGADORES");
+        window.show();
+    }
+
+    @FXML
+    public void iniciarPartida(ActionEvent event2) throws IOException {
+        Stage window2 = (Stage) ((Node) event2.getSource()).getScene().getWindow();
+
+        window2.setScene(new Scene(FXMLLoader.load(getClass().getResource("../fxmls/Tabuleiro.fxml"))));
+        window2.setTitle("ChessQMate - TABULEIRO");
+        window2.show();
+    }
 }
