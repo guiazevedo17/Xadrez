8c3ad0a2ba888764fc41c803756606664479534f
guiazevedo17 <gui.azevedo@ymail.com>
2021-12-03 01:48:49 -0300

    tela de menu criada

---
+++ src/classes/Menu.java
@@ -0,0 +1,23 @@
+package classes;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Menu extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("ChessQMate - MENU");
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../fxmls/Menu.fxml"))));
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("../imagens/icon.png")));
+        stage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+}
